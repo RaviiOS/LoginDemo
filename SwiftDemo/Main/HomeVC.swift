177d54a8483831be5159dd9eb54b17c7e4a05625
Ravi <yaganti.ravikumar@gmail.com>
2019-12-10 22:04:45 +0530

    Fixed version issues

--- SwiftDemo/Main/HomeVC.swift
+++ SwiftDemo/Main/HomeVC.swift
@@ -21,14 +21,4 @@ class HomeVC: UIViewController {
         UIApplication.setRootView(LoginVC.instantiate(fromAppStoryboard: .PreLogin), options: UIApplication.logoutAnimation)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
